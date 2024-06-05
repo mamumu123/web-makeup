f2159a0e8939d2dbbf17d85b71c7400942d8effd
lem <11597313+lemC@user.noreply.gitee.com>
2024-06-05 20:57:41 +0800

    chore: compress

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -2,7 +2,8 @@
 
 import { Button } from "flowbite-react";
 import { useRouter } from "next/navigation";
-
+import { ImgComparisonSlider } from '@img-comparison-slider/react';
+import Image from 'next/image';
 
 export default function Home() {
 
@@ -18,8 +19,15 @@ export default function Home() {
       <div className='font-bold text-4xl text-center text-black mb-10'>在线变装</div>
       <div className="w-full h-[50%] flex flex-col items-center">
         <h2 className="mb-4">上传一张人像照片，就可以开始神奇变化</h2>
-        {/* 展示一些效果 demo, */}
-        <Button onClick={gotoMakeup}>去改造</Button>
+        <div className={'relative w-[600px] h-[600px] flex items-center justify-center overflow-hidden'}>
+          {/* 展示一些效果 demo, */}
+          <ImgComparisonSlider className={'absolute top-0 left-0'}>
+            <Image slot="first" alt='first' src="/examples/a.png" width={600} height={600} />
+            <Image slot="second" alt='second' src="/out/out.png" width={600} height={600} />
+          </ImgComparisonSlider>
+        </div>
+
+        <Button onClick={gotoMakeup} className="mt-5">动手试试</Button>
       </div>
     </div>
   );
